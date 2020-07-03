4181e5f334f9f22b164e7d0f0c67830061faa2be
André Lam <anonymoustrooper@protonmail.com>
2020-07-03 20:45:13 +0200

    Hp files included

--- source/hp.sh
+++ source/hp.sh
@@ -5,7 +5,7 @@ if [ $# -eq 1 ]
   echo "%%HP: T(1)A(R)F(.);" > $1
     bbe -e "s/\x3C\x3C/\xAB/" $1.s|bbe -e "s/\x3E\x3E/\xBB/"|bbe -e "s/\x2D\x3E/\x8D/"|bbe -e "s/\x3C\x43\x52\x3E/\x0A/"|bbe -e "s/\x3C\x50\x49\x3E/\x87/"|bbe -e "s/\x3C\x53\x51\x52\x54\x3E/\x83/"  >> $1
     sudo kermit settings-hp50g -l /dev/ttyUSB0 -s $1
-    rm $1
+    #rm $1
   exit 0
 fi
 echo "Error you have t give 1 parameter, the xx.s without the s!"
